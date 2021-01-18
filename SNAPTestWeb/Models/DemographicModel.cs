f07bdcd02ace8df33b98fd6e7e9bf2a0335432a4
michael watson <m.watson.tn@gmail.com>
2021-01-18 16:10:55 -0600

    added basic models to begin building out views

---
+++ SNAPTestWeb/Models/DemographicModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SNAPTestWeb.Models
+{
+    public class DemographicModel
+    {
+        public string ZipCode { get; set; }
+        public int HouseholdSize { get; set; }
+
+    }
+}
