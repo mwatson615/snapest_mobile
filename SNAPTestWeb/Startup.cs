2b85cfec52e5bd3fe26d0dc4b99dc0332227f919
michael watson <m.watson.tn@gmail.com>
2021-01-18 16:37:32 -0600

    set up routing and home page load

--- SNAPTestWeb/Startup.cs
+++ SNAPTestWeb/Startup.cs
@@ -16,6 +16,8 @@ namespace SNAPTestWeb
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +27,15 @@ namespace SNAPTestWeb
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
